585830367974829fbf42b00a2183c95f0bb89093
Xiaolong Han <kpkhxlgy0@163.com>
2016-03-07 17:56:07 +0800

    x

---
+++ src/agent/main.go
@@ -0,0 +1,158 @@
+package main
+
+import (
+	"encoding/binary"
+	log "github.com/gonet2/libs/nsq-logger" // nsq based logging
+	"io"
+	"net"
+	"net/http"
+	_ "net/http/pprof"
+	"os"
+	"time"
+)
+
+import (
+	. "types"
+	"utils"
+)
+
+const (
+	_port = ":8888" // the incoming address for this agent, you can use docker -p to map ports
+)
+
+const (
+	SERVICE = "[AGENT]"
+)
+
+func main() {
+	// to catch all uncaught panic
+	defer utils.PrintPanicStack()
+
+	// open profiling
+	go func() {
+		log.Info(http.ListenAndServe("0.0.0.0:6060", nil))
+	}()
+
+	// set log prefix
+	log.SetPrefix(SERVICE)
+
+	// resolve address & start listening
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", _port)
+	checkError(err)
+
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	checkError(err)
+
+	log.Info("listening on:", listener.Addr())
+
+	// startup
+	startup()
+
+LOOP:
+	// loop accepting
+	for {
+		conn, err := listener.AcceptTCP()
+		if err != nil {
+			log.Warning("accept failed:", err)
+			continue
+		}
+		go handleClient(conn) // start a goroutine for every incoming connection for reading
+
+		// check server close signal
+		select {
+		case <-die:
+			listener.Close()
+			break LOOP
+		default:
+		}
+	}
+
+	// server closed, wait forever
+	// other options:
+	// select{} 	-- may cause deadlock detected error, not tested yet
+	for {
+		<-time.After(time.Second)
+	}
+}
+
+// PIPELINE #1: handleClient
+// the goroutine is used for reading incoming PACKETS
+// each packet is defined as :
+// | 2B size |     DATA       |
+//
+func handleClient(conn *net.TCPConn) {
+	defer utils.PrintPanicStack()
+	// set socket read buffer
+	conn.SetReadBuffer(SO_RCVBUF)
+	// set socket write buffer
+	conn.SetWriteBuffer(SO_SNDBUF)
+
+	// for reading the 2-Byte header
+	header := make([]byte, 2)
+	// the input channel for agent()
+	in := make(chan []byte)
+	defer func() {
+		close(in) // session will close
+	}()
+
+	// create a new session object for the connection
+	// and record it's IP address
+	var sess Session
+	host, port, err := net.SplitHostPort(conn.RemoteAddr().String())
+	if err != nil {
+		log.Error("cannot get remote address:", err)
+		return
+	}
+	sess.IP = net.ParseIP(host)
+	log.Infof("new connection from:%v port:%v", host, port)
+
+	// session die signal, will be triggered by agent()
+	sess.Die = make(chan struct{})
+
+	// create a write buffer
+	out := new_buffer(conn, sess.Die)
+	go out.start()
+
+	// start agent for PACKET processing
+	wg.Add(1)
+	go agent(&sess, in, out)
+
+	// read loop
+	for {
+		// solve dead link problem:
+		// physical disconnection without any communcation between client and server
+		// will cause the read to block FOREVER, so a timeout is a rescue.
+		conn.SetReadDeadline(time.Now().Add(TCP_READ_DEADLINE * time.Second))
+
+		// read 2B header
+		n, err := io.ReadFull(conn, header)
+		if err != nil {
+			log.Warningf("read header failed, ip:%v reason:%v size:%v", sess.IP, err, n)
+			return
+		}
+		size := binary.BigEndian.Uint16(header)
+
+		// alloc a byte slice of the size defined in the header for reading data
+		payload := make([]byte, size)
+		n, err = io.ReadFull(conn, payload)
+		if err != nil {
+			log.Warningf("read payload failed, ip:%v reason:%v size:%v", sess.IP, err, n)
+			return
+		}
+
+		// deliver the data to the input queue of agent()
+		select {
+		case in <- payload: // payload queued
+		case <-sess.Die:
+			log.Warningf("connection closed by logic, flag:%v ip:%v", sess.Flag, sess.IP)
+			return
+		}
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Critical(err)
+		os.Exit(-1)
+	}
+}
---
+++ src/agent/proxy.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	log "github.com/gonet2/libs/nsq-logger"
+	"github.com/peterbourgon/g2s"
+)
+
+import (
+	"client_handler"
+	"misc/packet"
+	. "types"
+	"utils"
+)
+
+const (
+	STATSD_PREFIX       = "API."
+	ENV_STATSD          = "STATSD_HOST"
+	DEFAULT_STATSD_HOST = "172.17.42.1:8125"
+)
+
+var (
+	_statter g2s.Statter
+)
+
+func init() {
+	addr := DEFAULT_STATSD_HOST
+	if env := os.Getenv(ENV_STATSD); env != "" {
+		addr = env
+	}
+
+	s, err := g2s.Dial("udp", addr)
+	if err != nil {
+		log.Critical(err)
+		os.Exit(-1)
+	}
+	_statter = s
+}
+
+// client protocol handle proxy
+func proxy_user_request(sess *Session, p []byte) []byte {
+	start := time.Now()
+	defer utils.PrintPanicStack(sess, p)
+	// 解密
+	if sess.Flag&SESS_ENCRYPT != 0 {
+		sess.Decoder.XORKeyStream(p, p)
+	}
+	// 封装为reader
+	reader := packet.Reader(p)
+
+	// 读客户端数据包序列号(1,2,3...)
+	// 客户端发送的数据包必须包含一个自增的序号，必须严格递增
+	// 加密后，可避免重放攻击-REPLAY-ATTACK
+	seq_id, err := reader.ReadU32()
+	if err != nil {
+		log.Error("read client timestamp failed:", err)
+		sess.Flag |= SESS_KICKED_OUT
+		return nil
+	}
+
+	// 数据包序列号验证
+	if seq_id != sess.PacketCount {
+		log.Errorf("illegal packet sequence id:%v should be:%v size:%v", seq_id, sess.PacketCount, len(p)-6)
+		sess.Flag |= SESS_KICKED_OUT
+		return nil
+	}
+
+	// 读协议号
+	b, err := reader.ReadS16()
+	if err != nil {
+		log.Error("read protocol number failed.")
+		sess.Flag |= SESS_KICKED_OUT
+		return nil
+	}
+
+	// 根据协议号断做服务划分
+	// 协议号的划分采用分割协议区间, 用户可以自定义多个区间，用于转发到不同的后端服务
+	var ret []byte
+	if b > MAX_PROTO_NUM {
+		if err := forward(sess, p[4:]); err != nil {
+			log.Errorf("service id:%v execute failed, error:%v", b, err)
+			sess.Flag |= SESS_KICKED_OUT
+			return nil
+		}
+	} else {
+		if h := client_handler.Handlers[b]; h != nil {
+			ret = h(sess, reader)
+		} else {
+			log.Errorf("service id:%v not bind", b)
+			sess.Flag |= SESS_KICKED_OUT
+			return nil
+		}
+	}
+
+	// 监控协议处理时间
+	// 监控数值会发送到statsd,格式为:
+	// API.XXX_REQ = 10ms
+	elasped := time.Now().Sub(start)
+	if b != 0 { // 排除心跳包日志
+		log.Trace("[REQ]", client_handler.RCode[b])
+		_statter.Timing(1.0, fmt.Sprintf("%v%v", STATSD_PREFIX, client_handler.RCode[b]), elasped)
+	}
+	return ret
+}
